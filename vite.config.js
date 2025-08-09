77a5ea547ef6d042f52dbc8a5f07f01b54e04f87
NAMEV2023 <achmad.23172@mhs.unesa.ac.id>
2025-08-09 22:03:28 +0700

    update 3

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui'],
-      },
-    },
-  },
-  plugins: [],
-}
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
